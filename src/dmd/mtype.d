f52892d4325ae8366cd9402a2f837c8f94da2923
Nick Treleaven <n@trelsoft.com>
2018-02-05 18:30:43 +0000

    WIP

--- src/dmd/mtype.d
+++ src/dmd/mtype.d
@@ -5955,15 +5955,14 @@ extern (C++) final class TypeFunction : TypeNext
 
         size_t nparams = Parameter.dim(parameters);
         size_t nargs = args ? args.dim : 0;
-        if (nparams == nargs)
-        {
-        }
-        else if (nargs > nparams)
+        if (nargs > nparams)
         {
             if (varargs == 0)
             {
-                if (pMessage) *pMessage = getMatchError("expected %d argument(s), not %d", nparams, nargs);
-                goto Nomatch;
+                // suppress early exit if an error message is wanted,
+                // so we can check any matching args are valid
+                if (!pMessage)
+                    goto Nomatch;
             }
             // too many args; no match
             match = MATCH.convert; // match ... with a "conversion" match level
@@ -6012,8 +6011,8 @@ extern (C++) final class TypeFunction : TypeNext
             {
                 if (p.defaultArg)
                     continue;
-                goto L1;
                 // try typesafe variadics
+                goto L1;
             }
             {
                 Expression arg = (*args)[u];
@@ -6196,7 +6195,8 @@ extern (C++) final class TypeFunction : TypeNext
                 if (pMessage && u < nargs)
                     *pMessage = getParamError("cannot pass argument %s to parameter %s", (*args)[u], p);
                 else if (pMessage)
-                    *pMessage = getMatchError("expected %d argument(s), not %d", nparams, nargs);
+                    *pMessage = getMatchError("missing argument for parameter `%s` (#%d/%d)",
+                        parameterToChars(p, this, false), u + 1, nparams);
                 goto Nomatch;
             }
             if (m < match)
@@ -6204,6 +6204,12 @@ extern (C++) final class TypeFunction : TypeNext
         }
 
     Ldone:
+        if (pMessage && !varargs && nargs > nparams)
+        {
+            // all parameters had a match, but there are surplus args
+            *pMessage = getMatchError("expected %d argument(s), not %d", nparams, nargs);
+            goto Nomatch;
+        }
         //printf("match = %d\n", match);
         return match;
 
