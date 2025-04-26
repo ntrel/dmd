15e66b13e53271de9316a3522c9c453db8a86b5d
Nick Treleaven <ntrel002@gmail.com>
2025-04-26 11:48:54 +0100

    Add UnpackExp to unpack an ExpStatement
    Ban enum/static components.

--- compiler/src/dmd/statement.d
+++ compiler/src/dmd/statement.d
@@ -382,6 +382,7 @@ extern (C++) class ExpStatement : Statement
         this.exp = exp;
     }
 
+    /// Use this in place of DeclarationStatement
     final extern (D) this(Loc loc, Dsymbol declaration) @safe
     {
         super(loc, STMT.Exp);
--- compiler/test/fail_compilation/unpacking.d
+++ compiler/test/fail_compilation/unpacking.d
@@ -2,15 +2,17 @@
 REQUIRED_ARGS: -preview=tuples -vcolumns
 TEST_OUTPUT:
 ---
-fail_compilation/unpacking.d(26,14): Error: unpacked variable `b` needs a type or at least one storage class, did you mean `auto b`?
-fail_compilation/unpacking.d(27,15): Error: unpacked variable `b` needs a type or at least one storage class, did you mean `auto b`?
-fail_compilation/unpacking.d(27,18): Error: unpacked variable `c` needs a type or at least one storage class, did you mean `auto c`?
-fail_compilation/unpacking.d(28,23): Error: unpacked variable `c` needs a type or at least one storage class, did you mean `auto c`?
-fail_compilation/unpacking.d(30,16): Error: variable name expected after type `(a,b)`, not `,`
-fail_compilation/unpacking.d(30,16): Error: found `,` instead of statement
-fail_compilation/unpacking.d(31,10): Error: unexpected identifier `a` in declarator
-fail_compilation/unpacking.d(31,17): Error: unexpected identifier `b` in declarator
-fail_compilation/unpacking.d(33,17): Error: expected identifier after type `int` in unpack declaration
+fail_compilation/unpacking.d(28,14): Error: unpacked variable `b` needs a type or at least one storage class, did you mean `auto b`?
+fail_compilation/unpacking.d(29,15): Error: unpacked variable `b` needs a type or at least one storage class, did you mean `auto b`?
+fail_compilation/unpacking.d(29,18): Error: unpacked variable `c` needs a type or at least one storage class, did you mean `auto c`?
+fail_compilation/unpacking.d(30,23): Error: unpacked variable `c` needs a type or at least one storage class, did you mean `auto c`?
+fail_compilation/unpacking.d(32,16): Error: variable name expected after type `(a,b)`, not `,`
+fail_compilation/unpacking.d(32,16): Error: declaration expected, not `,`
+fail_compilation/unpacking.d(33,10): Error: unexpected identifier `a` in declarator
+fail_compilation/unpacking.d(33,17): Error: unexpected identifier `b` in declarator
+fail_compilation/unpacking.d(33,19): Error: variable name expected after type `(int,int)`, not `,`
+fail_compilation/unpacking.d(33,19): Error: declaration expected, not `,`
+fail_compilation/unpacking.d(35,17): Error: expected identifier after type `int` in unpack declaration
 ---
 */
 
@@ -21,7 +23,7 @@ struct Tuple(T...)
 }
 auto tuple(T...)(T args) => Tuple!T(args);
 
-void main()
+private // test parser errors
 {
     (int a, b) = tuple(1, "2"); // error
     (int a, (b, c)) = tuple(1, tuple("2", 3.0)); // error
