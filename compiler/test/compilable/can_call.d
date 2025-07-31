8fea3b1ad8007f0cb5dd043ab98afe5c7af47e8c
Nick Treleaven <ntrel002@gmail.com>
2025-07-31 20:05:21 +0100

    Update tests

--- compiler/test/compilable/can_call.d
+++ compiler/test/compilable/can_call.d
@@ -2,26 +2,42 @@ void f(int) @system
 {
     void s() @safe
     {
-        // safe false?
-        static assert(__traits(canCall, f, 1));
+        //FIXME static assert(!__traits(canCall, f, 1));
     }
 }
-
 static assert(!__traits(canCall, f));
 static assert(__traits(canCall, f, 1));
 static assert(!__traits(canCall, f, 1, 2));
 static assert(!__traits(canCall, f, ""));
 
+// lvalue argument
 void g(ref int);
 int i;
-
 static assert(!__traits(canCall, g, 1));
 static assert(__traits(canCall, g, i));
 
+// method
 struct S
 {
     void f(int);
 }
-
 static assert(!__traits(canCall, S.f));
 static assert(__traits(canCall, S.f, 1));
+
+// IFTI
+void h()() {
+    //TODO ignore body errors
+    //invalid;
+};
+static assert(__traits(canCall, h));
+static assert(!__traits(canCall, h, 1));
+
+void j(T)(T);
+static assert(!__traits(canCall, j));
+static assert(__traits(canCall, j, 1));
+static assert(!__traits(canCall, j!int, ""));
+
+// lambda
+alias k = a => 3;
+static assert(!__traits(canCall, k));
+static assert(__traits(canCall, k, 1));
