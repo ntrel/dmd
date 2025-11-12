f57e01ec2443a1ab6ae906ebdda70ec1db852215
Nick Treleaven <ntrel002@gmail.com>
2025-11-12 10:16:40 +0000

    test nested UnpackDeclaration
    Tweak tests.

--- compiler/test/runnable/unpack_assign.d
+++ compiler/test/runnable/unpack_assign.d
@@ -11,16 +11,23 @@ auto tuple(T...)(T args) => Tuple!T(args);
 
 void main()
 {
-    string b, d;
-    double e, h;
-
+    string b;
     (int a, b) = tuple(1, "2");
     assert(a == 1);
     assert(b == "2");
 
-    (int c, (d, e)) = tuple(1, tuple("2", 3.0));
-    (int f, (auto g, h)) = tuple(1, tuple("2", 3.0));
+    // nested UnpackExp
+    string d;
+    double e, h;
+    (int c, (d, e)) = tuple(1, tuple("4", 3.0));
+    (int f, (auto g, h)) = tuple(1, tuple("4", 3.0));
     assert(c == 1 && f == c);
-    assert(d == "2" && g == d);
+    assert(d == "4" && g == d);
     assert(e == 3.0 && h == e);
+
+    // nested UnpackDeclaration
+    int i;
+    (auto (j,), i) = tuple(tuple("5"), 1);
+    assert(j == "5");
+    assert(i == 1);
 }
